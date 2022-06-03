4827525d3ea83fa1cc7d0a33118f97deb0c687a6
Dân Thế Nguyễn <dan.nguyen200206@vnuk.edu.vn>
2022-06-03 09:09:50 +0700

    updated NavBar

---
+++ src/components/NavBar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar-main">
+        <div className="navbar-container">
+          <h1>FrontEndMasters</h1>
+          <h2>.Blog</h2>
+          <div className="navbar-button-layout">
+            
+          </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className="app-home-main">
+        <div className="app-home-main-container">
+            <div className="app-home-text-container">
+                <h1>Welcome To</h1>
+                <h1>FrontEndMaster -</h1>
+                <h1>Front End Dev</h1>
+
+                <div className="text-container-description">
+                    <div className="vertical-line"/>
+                    <p>On this blog I share tips and tricks, frameworks, projects, tutorials, etc Make sure you subscribe to get the latest updates</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
