dbe4841a1473d36cd7432421ad97087b015f3d4a
Dân Thế Nguyễn <dan.nguyen200206@vnuk.edu.vn>
2022-06-07 19:35:22 +0700

    optimize folders

---
+++ src/constants/images.js
@@ -0,0 +1,9 @@
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
+export {
+    TwitterIcon,
+    InstagramIcon,
+    LinkedInIcon,
+};
